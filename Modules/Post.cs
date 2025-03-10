942a1de6eeb429e99152c851e31c7c22f6267d30
ben9955 <ben95ansari@gmail.com>
2025-03-10 15:18:03 +0000

    Refactored Post model to follow C# naming conventions

--- Modules/Post.cs
+++ Modules/Post.cs
@@ -3,21 +3,22 @@ using System;
 public class Post
 {
     public int Id { get; set; }
-    public int user_id { get; set; } // Foreign key reference to User
-    public string title { get; set; }
-    public string content { get; set; }
-    public string media_url { get; set; } // Stores file path of the image
-    public int amount_gained { get; set; }
+    public int UserId { get; set; } // Foreign key reference to User
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string MediaUrl { get; set; } // Stores file path of the image
+    public int AmountGained { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
     public Post() { }
-    public Post(int userId, string title, string content, string Media_url, int AmountGained)
+
+    public Post(int userId, string title, string content, string mediaUrl, int amountGained)
     {
-        this.user_id = userId;
-        this.title = title;
-        this.content = content;
-        this.media_url = Media_url;
-        this.amount_gained = AmountGained;
+        this.UserId = userId;
+        this.Title = title;
+        this.Content = content;
+        this.MediaUrl = mediaUrl;
+        this.AmountGained = amountGained;
     }
 }
