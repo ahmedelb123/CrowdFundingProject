2b0e7f1324fd3c34a6540cf03ecdc5b47e73c520
Ahmed Elbehri <ahmedelb0101@gmail.com>
2025-03-13 20:21:53 +0000

    Merged donation branch and updated project files

--- obj/Debug/net8.0/CroundFundingProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/CroundFundingProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CroundFundingProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+136651604296585afcc6a637c00613c30d6c68f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12607be22a90b177a6b60b9fac12e3e0774345ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("CroundFundingProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CroundFundingProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
